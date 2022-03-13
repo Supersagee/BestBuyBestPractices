4c6717fcaff4f2bd36802e0ef7982ff5242c9da3
Supersagee <williamdsage@gmail.com>
2022-03-13 16:48:23 -0500

    complete

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM Products;");
+        }
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+                new { name = name, price = price, categoryID = categoryID });
+               
+        }
+
+        public void UpdateProduct(int productID, string name)
+        {
+            _connection.Execute("UPDATE products SET Name = @name WHERE ProductID = @productID;",
+                new { name = name, productID = productID });
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID });
+        }
+    }
+}
